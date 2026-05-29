0004e548439ac62d357583677735d416824ff59b
unknown <vijayadirgachandan@gmail.com>
2026-05-29 10:14:45 +0530

    first commit

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn your project name.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn django_deploye.wsgi:application --bind 0.0.0.0:$PORT
